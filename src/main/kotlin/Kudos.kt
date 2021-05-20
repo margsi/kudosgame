3935318477d3b37235262adf2dc545d1be3f17c8
Rick Spiegl <Rick.Spiegl@cloudflight.io>
2021-05-20 17:50:27 +0200

    Remove unneeded data from class

--- src/main/kotlin/Kudos.kt
+++ src/main/kotlin/Kudos.kt
@@ -3,10 +3,6 @@ import java.time.LocalDateTime
 class Kudos(
     val id: Long,
     val sender_id: Long?,
-    val creation_date: String,
-    val content: String,
-    val organization_unit_id: Long?,
     val person_id: Long?,
-    val checked: Boolean
 ) {
 }

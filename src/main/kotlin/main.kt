4a4a0ba7fbb83f01d4b78909fd6e1bf6044fe7db
Andreas Parteli <Andreas.Parteli@cloudflight.io>
2021-05-20 18:12:32 +0200

    add call

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,9 +4,26 @@ import java.io.File
 
 fun main(args: Array<String>) {
 
-    val json =File(System.getProperty("user.dir") + "\\resources\\" + "kudos.json").readText()
-    val kudos = Gson().fromJson<List<Kudos>>(json)
-    println(kudos)
+    val json = File(System.getProperty("user.dir") + "\\resources\\" + "kudos.json").readText()
+    var kudos = Gson().fromJson<List<Kudos>>(json)
+    kudos = kudos.filter { it.person_id != null }
+
+//    val personIndex = kudos.mapIndexed { i, k -> k.person_id to i }.toMap()
+    val groupedBySender = kudos.mapIndexed { i, k -> Pair(i, k) }.groupBy { k -> k.second.sender_id }.toMap()
+
+    val g = Graph(kudos.size)
+
+    kudos.forEachIndexed { graphStart, k ->
+//        if (k.person_id == null) {
+//            return
+//        }
+        groupedBySender[k.person_id]?.forEach { targetKudoPair ->
+            g.addEdge(graphStart, targetKudoPair.first, 1)
+
+        }
+    }
+    val path = g.longestPath(0)
+    print(path)
 }
 
 inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object: TypeToken<T>() {}.type)

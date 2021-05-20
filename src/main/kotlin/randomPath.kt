26cbf38611f318b5ae2590cb2c8402e0451ac58f
Simon Margreiter <Simon.Margreiter@cloudflight.io>
2021-05-20 19:01:13 +0200

    Random paths

--- src/main/kotlin/randomPath.kt
+++ src/main/kotlin/randomPath.kt
@@ -5,18 +5,19 @@ fun main(args: Array<String>) {
     val json = File(System.getProperty("user.dir") + "\\resources\\" + "kudos.json").readText()
     var kudos = Gson().fromJson<List<Kudos>>(json)
     var highScore = 0
-    (1L..100L).forEach {
+    (0..99).forEach {
         val path = findPath(kudos,it)
         if(path.size > highScore){
             println(path.joinToString())
+            highScore = path.size
         }
     }
 }
 
-fun findPath(kudosList: List<Kudos>, startId: Long): List<Long> {
+fun findPath(kudosList: List<Kudos>, startIndex: Int): List<Long> {
     val kudos = kudosList.toMutableList()
-    val path = mutableListOf<Long>(startId)
-    var currentKudo = kudos.find { it.id == startId }
+    var currentKudo = kudos[startIndex]
+    val path = mutableListOf<Long>(currentKudo.id)
     kudos.remove(currentKudo)
     var possibleNextSteps = kudos.filter { it.sender_id == currentKudo!!.person_id }
     while (possibleNextSteps.isNotEmpty()) {
